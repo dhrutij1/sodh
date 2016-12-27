111adec3a6c824e69e01e7b2b667365d9dca9d43
abhishekpanchal <dhruti.joshi@sooryen.com>
2016-12-27 18:08:33 +0530

    initial

---
+++ src/main/java/com/sodh/SodhApplication.java
@@ -0,0 +1,12 @@
+package com.sodh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodhApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sodh/SodhApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sodh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SodhApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
